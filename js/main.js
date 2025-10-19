5f9d5e1f789ceda4bc51ab3a07e2569f66996933
Ramil Zaripov <zarram89@gmail.com>
2025-10-20 03:59:37 +0500

    feat: m12t1 добавялет фильтры

--- js/main.js
+++ js/main.js
@@ -2,35 +2,35 @@ import {renderPictures} from './pictures.js';
 import {renderBigPictureElement} from './big-picture.js';
 import {fetchPictures} from './api.js';
 import {showDataError} from './messages.js';
+import {initFilters} from './filters.js';
 import './upload-form.js';
-import './upload-effects'
+import './upload-effects';
 
 const picturesListElement = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const filters = document.querySelector('.img-filters');
 
 let picturesData = [];
 
-function activateFilters() {
-  filters.classList.remove('img-filters--inactive');
-  // фильтры позже (дребезг 500 мс по ТЗ)
-}
 
 function attachOpenHandler() {
 // 1 Навешиваем только один обработчик на весь контейнер
   picturesListElement.addEventListener('click', (evt) => {
-    // Проверяем, кликнули ли по миниатюре или по img внутри неё
+    // 2Проверяем, кликнули ли по миниатюре или по img внутри неё
     const target = evt.target.closest('.picture');
 
-    // 2 если клик не по миниатюре — выходим
-    if (!target) return;
+    // 3 если клик не по миниатюре — выходим
+    if (!target) {
+      return;
+    }
 
-    // 3️ Получаем id и находим соответствующий объект
+    // 4 Получаем id и находим соответствующий объект
     const id = Number(target.dataset.pictureId);
     const pictureData = picturesData.find((item) => item.id === id);
 
-    // 4️ Показываем модалку с изображением
-    if (pictureData) renderBigPictureElement(pictureData);
+    // 5 Показываем модалку с изображением
+    if (pictureData) {
+      renderBigPictureElement(pictureData);
+    }
   });
 }
 
@@ -38,12 +38,11 @@ async function init() {
   try {
     picturesData = await fetchPictures();
     renderPictures(picturesData, picturesListElement, pictureTemplate);
-    activateFilters();
+    initFilters(picturesData, picturesListElement, pictureTemplate);
     attachOpenHandler();
   } catch (e) {
-    console.error('Load error:', e);
     showDataError();
   }
 }
 
-init();
+await init();
