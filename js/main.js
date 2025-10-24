39b570f0da019dd51dd269da2b4025ee4f84ca9b
Ramil <zarram89@gmail.com>
2025-10-24 18:50:40 +0500

    feat: adds api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
-import './form.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { setOnFormSubmit, hideModal } from './form.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
 
-renderPictures(getPictures());
+const onSendDataSuccess = () => {
+  hideModal();
+  showSuccessMessage();
+};
+
+const onSendDataError = () => {
+  showErrorMessage();
+};
+
+setOnFormSubmit(async (data) => {
+  await sendData(onSendDataSuccess, onSendDataError, data);
+});
+
+getData(renderPictures, showAlert);
--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -10,4 +12,28 @@ const checkStringLength = (string, length) => string.length <= length;
 const getRandomArrayElement = (array) =>
   array[getRandomPositiveInteger(0, array.length - 1)];
 
-export { getRandomPositiveInteger, checkStringLength, getRandomArrayElement };
+const showAlert = (message) => {
+  const alert = document.createElement('div');
+  alert.style.position = 'absolute';
+  alert.style.zIndex = '100';
+  alert.style.left = '0';
+  alert.style.top = '0';
+  alert.style.right = '0';
+  alert.style.padding = '10px 3px';
+  alert.style.fontSize = '30px';
+  alert.style.textAlign = 'center';
+  alert.style.backgroundColor = 'red';
+  alert.textContent = message;
+  document.body.append(alert);
+
+  setTimeout(() => {
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {
+  getRandomPositiveInteger,
+  checkStringLength,
+  getRandomArrayElement,
+  showAlert,
+};
